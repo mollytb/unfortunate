2fce35b8c382019582326713fb0c4a1c71eef14a
Molly Burns <Molly.t.burns@gmail.com>
2018-06-27 18:57:50 -0400

    moved words

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Unfortunate Cookies</h1>
+          <img src={logo} className="App-logo" alt="logo" />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
