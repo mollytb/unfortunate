35d7e9736c0a392837dc5252403c868185c136b0
Unknown <acwolf@live.com>
2018-06-27 19:48:05 -0400

    add Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './fortune_cookie_whole.png';
 import './App.css';
+import NavBar from "./components/NavBar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <NavBar />
         <header className="App-header">
           <h1 className="App-title">Unfortunate Cookies</h1>
           <img src={logo} className="App-logo" alt="logo" />
--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,1 +1,25 @@
 //stuff for NavBar component
+import React from "react";
+
+const NavBar = () => (
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand" href="#">Unfortunate</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+      </li>
+    </ul>
+    <form className="form-inline my-2 my-lg-0">
+      <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+);
+
+export default NavBar;
