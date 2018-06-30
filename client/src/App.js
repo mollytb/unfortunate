eb178c01de26c08d0a2ff4d74fa8fed33cb6d6f6
Molly Burns <Molly.t.burns@gmail.com>
2018-06-30 11:25:51 -0400

    editing Cookie component

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import logo from './fortune_cookie_whole.png';
+//import logo from './fortune_cookie_whole.png';
 import './App.css';
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import Cookie from "./components/Cookie";
+
 
 class App extends Component {
   render() {
     return (
+      <body>
       <div className="App">
       <NavBar />
-      <body className="Body">
-        <div className="Cookie">
-          <img src={logo} class="img-fluid" alt="logo" />
-          </div>
-        <p>
+      
+      <Cookie /> 
+        <p>I'm annoyed, is anyone reading this?
         </p>
         
         <Footer />
       
-      </body>
+      
       </div>
+      </body>
     );
   }
 }
--- client/src/components/Cookie/Cookie.js
+++ client/src/components/Cookie/Cookie.js
@@ -1,8 +1,10 @@
 //stuff for cookie component
-
+import logo from './fortune_cookie_whole.png';
 import React from "react";
 
 const Cookie = () => (
-
+    <div className="Cookie">
+    <img src={logo}  alt="logo" />
+    </div>
 )
 export default Cookie;
