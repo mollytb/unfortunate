fb4f7149dac46fff04bb44dffa7b7ad427d38a3c
Unknown <andrewrussmancuso@gmail.com>
2018-06-30 11:48:16 -0400

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 31c3d9fb593e9be1ba76c1ea389e7d974c06642f.

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,23 @@
 import React, { Component } from 'react';
-//import logo from './fortune_cookie_whole.png';
+import logo from './fortune_cookie_whole.png';
 import './App.css';
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
-import Cookie from "./components/Cookie";
-
 
 class App extends Component {
   render() {
     return (
-      <body>
       <div className="App">
       <NavBar />
-<<<<<<< HEAD
       <body className="Body">
         <div className="Cookie">
           <img src={logo} class="img-fluid" alt="logo" />
           </div>
         <p>
-=======
-      
-      <Cookie /> 
-        <p>I'm annoyed, is anyone reading this?
->>>>>>> master
         </p>
-        
+        </body>
         <Footer />
-      
-      
       </div>
-      </body>
     );
   }
 }
