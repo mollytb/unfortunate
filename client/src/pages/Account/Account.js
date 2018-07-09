c5510ea4f17f9594a5a45c60dcd20e599ffb2c90
Unknown <acwolf@live.com>
2018-07-09 14:42:53 -0400

    account page setup

--- client/src/pages/Account/Account.js
+++ client/src/pages/Account/Account.js
@@ -21,14 +21,35 @@ class Account extends Component {
                 <p className="card-text">Dynamically add favortied fortunes??</p>
               </div>
             </div>
-          </div>
-          <p>Create a New Unfortune? Is that going on this page at all?</p>
-          <p>Your Unfortunes</p>
-      </div>
-      <Footer />
-      </div >
-    );
-  }
-}
+            <div className="card">
+              <h5 className="card-header">Your Unfortunes</h5>
+              <div className="card-body">
+                <p className="card-text">Fortunes you've created will display here</p>
+              </div>
+            </div>
 
+            <div className="card">
+            <h5 className="card-header">Create new Unfortune</h5>
+            <form>
+
+              <div className="form-group card-body">
+                <label for="exampleFormControlInput1">Email address</label>
+                <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"></input>
+              </div>
+                <div className="form-group card-body">
+                  <label for="exampleFormControlTextarea1">Example textarea</label>
+                  <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+
+            </form>
+            <p>add button and connect FormBtn here?? </p>
+            </div>
+          </div>
+          </div>
+          <Footer />
+        </div >
+        );
+      }
+    }
+    
 export default Account;
