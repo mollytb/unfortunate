b31162c03f10e2f630b85560e109dcd0065e1901
Unknown <acwolf@live.com>
2018-07-09 16:50:02 -0400

    navbar edits

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -13,8 +13,10 @@ const NavBar = () => (
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav mr-auto">
       <li className="nav-item active">
-
         <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Account <span class="sr-only">(current)</span></a>
       </li>
     </ul>
     <form className="form-inline my-2 my-lg-0">
