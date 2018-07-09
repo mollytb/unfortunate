250f85ca9e6c5ed271978fb8aa2dfce279216e25
Molly Burns <Molly.t.burns@gmail.com>
2018-07-09 18:52:44 -0400

    more footer stuff

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,8 +5,7 @@ import "./Footer.css"
 
 const Footer = () => (
     <footer className="footer">
-<p>Collaboraters</p>
-<p><a href="http://www.andrewrmancuso.com/" target="blank">Andrew</a> | <a href="https://github.com/216anna" target="blank">Anna</a> | <a href="https://github.com/mollytb" target="blank">Molly</a> | <a href="https://github.com/darkzero-omega2003" target="blank">T.J.</a></p>
+<p>Collaboraters <a href="http://www.andrewrmancuso.com/" target="blank">Andrew</a> | <a href="https://github.com/216anna" target="blank">Anna</a> | <a href="https://github.com/mollytb" target="blank">Molly</a> | <a href="https://github.com/darkzero-omega2003" target="blank">T.J.</a></p>
     </footer>
 )
 export default Footer;
