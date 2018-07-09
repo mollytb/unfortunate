59489df00a5ac5143595e50788084e5a9e046947
Unknown <acwolf@live.com>
2018-07-09 14:23:15 -0400

    updated account page
    Need to adjust the font color. Words are faded/light.

--- client/src/pages/Account/Account.js
+++ client/src/pages/Account/Account.js
@@ -7,24 +7,28 @@ import { Input, FormBtn } from "../../components/Form";
 
 class Account extends Component {
 
-    render() {
-        return (
-        <div>
-          <div className="App">
+  render() {
+    return (
+      <div>
+        <div className="App">
           <NavBar />
           <p className="App-title">Account page! Change me!
             </p>
           <div>
-              <p>Create a New Unfortune? Is that going on this page at all?</p>
-              <p>Your Unfortunes</p>
-              <p>Favorite Unfortunes</p>
+            <div className="card">
+              <h5 className="card-header">Favorite Unfortunes</h5>
+              <div className="card-body">
+                <p className="card-text">Dynamically add favortied fortunes??</p>
+              </div>
+            </div>
           </div>
-        
-          </div>
-           <Footer />
-         </div>
-        );
-      }
+          <p>Create a New Unfortune? Is that going on this page at all?</p>
+          <p>Your Unfortunes</p>
+      </div>
+      <Footer />
+      </div >
+    );
+  }
 }
 
 export default Account;
