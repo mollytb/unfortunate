36fec77976f7b8f01b948c6c5f93182ee1b7703a
Molly Burns <Molly.t.burns@gmail.com>
2018-07-10 15:00:41 -0400

    link home from broken cookie

--- client/src/components/BrokenCookie/BrokenCookie.js
+++ client/src/components/BrokenCookie/BrokenCookie.js
@@ -2,13 +2,16 @@
 
 import React from "react";
 import logo2 from './cookie_right_paper.png';
-import "./BrokenCookie.css"
+import "./BrokenCookie.css";
+import {Link} from "react-router-dom";
 
 const BrokenCookie = (props) => (
     <div className="BrokenCookie">
+    <Link to ="/Home">
     -------This is where our fortune will go as of now------
         {props.fortune}
     <img className="size2" src={logo2}  alt="logo" />
+    </Link>
     </div>
 )
 export default BrokenCookie;
