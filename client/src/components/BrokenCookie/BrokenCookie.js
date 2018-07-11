a8bcb169c7a5737c586e1ef1a343c32fdbf443df
Molly Burns <Molly.t.burns@gmail.com>
2018-07-11 19:16:31 -0400

    Fortune moving

--- client/src/components/BrokenCookie/BrokenCookie.js
+++ client/src/components/BrokenCookie/BrokenCookie.js
@@ -6,12 +6,15 @@ import "./BrokenCookie.css";
 import {Link} from "react-router-dom";
 
 const BrokenCookie = (props) => (
+    
     <div className="BrokenCookie">
     <Link to ="/Home">
     -------This is where our fortune will go as of now------
         {props.fortune}
     <img className="size2" src={logo2}  alt="logo" />
+    
     </Link>
     </div>
+    
 )
 export default BrokenCookie;
