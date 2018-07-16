35fc02216d4eb8ef6e2a58c005dab6a7027d9b3a
Molly Burns <Molly.t.burns@gmail.com>
2018-07-16 19:05:20 -0400

    new pages

---
+++ client/src/pages/FortuneID/FortuneID.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import NavBar from "../../components/NavBar";
+import Cookie from "../../components/Cookie";
+import Footer from "../../components/Footer";
+import API from "../../utils/API";
+import { Input, FormBtn } from "../../components/Form";
+
+class FortuneID extends Component {
+
+    render() {
+        return (
+        <div>
+          <div className="App">
+          <NavBar />
+          <p className="App-title">Click the cookie...
+            </p>
+          <Cookie />
+
+        <div>
+        </div>
+          <div>
+            <Footer />
+          </div>
+         </div>
+       </div>
+        );
+      }
+}
+
+export default FortuneID;
